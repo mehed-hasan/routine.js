150ad51650d4616729987a0dfb0ad4a7e06df376
mehed-hasan <mehedi.hasan@intelli.global>
2023-05-15 12:47:58 +0600

    Routin 1.2 solved version !

--- routin.js
+++ routin.js
@@ -1,3 +1,8 @@
+// let at = ["first", "second", "third"];
+// at = at.filter((item) => item !== "second");
+// at.push("second");
+// console.log(at);
+
 let classes = [
   {
     className: 1,
@@ -8,34 +13,33 @@ let classes = [
       { name: "art", weekly: 1 },
     ],
     classPerDay: 3,
-    days: [
-      { name: "Satarday", sub: [] },
-      { name: "Sunday", sub: [] },
-      { name: "Monday", sub: [] },
-      { name: "Tuesday", sub: [] },
-      { name: "Wednesday", sub: [] },
-      { name: "Thursday", sub: [] },
-    ],
+    days: [{ name: "Satarday", sub: [] }],
   },
   {
     className: 2,
     subs: [
       { name: "ban", weekly: 6 },
-      { name: "religion", weekly: 6 },
+      { name: "eng", weekly: 6 },
       { name: "science", weekly: 6 },
-      { name: "eng", weekly: 5 },
+      { name: "religion", weekly: 5 },
       { name: "math", weekly: 6 },
       { name: "art", weekly: 1 },
     ],
     classPerDay: 5,
-    days: [
-      { name: "Satarday", sub: [] },
-      { name: "Sunday", sub: [] },
-      { name: "Monday", sub: [] },
-      { name: "Tuesday", sub: [] },
-      { name: "Wednesday", sub: [] },
-      { name: "Thursday", sub: [] },
+    days: [{ name: "Satarday", sub: [] }],
+  },
+  {
+    className: 3,
+    subs: [
+      { name: "ban", weekly: 6 },
+      { name: "eng", weekly: 6 },
+      { name: "science", weekly: 6 },
+      { name: "religion", weekly: 5 },
+      { name: "math", weekly: 6 },
+      { name: "art", weekly: 1 },
     ],
+    classPerDay: 5,
+    days: [{ name: "Satarday", sub: [] }],
   },
 
   //   ...
@@ -43,17 +47,11 @@ let classes = [
 
 let response = "";
 
-classes.forEach((singleClass = element) => {
+classes.forEach((singleClass = element, classesIndex) => {
   //Set subject for one day------------------------------
   //   -------------------------------------------------------
   // ----------------------------------------------------------
-  singleClass.days.forEach((singleDay = element) => {
-    // This code is for random sorting array so that always same series do not appear to choose subject
-    // singleClass.subs.sort(() =>
-    //   Math.floor(Math.random() * singleClass.subs.length)
-    // );
-    // This code is for random sorting array so that always same series do not appear to choose subject
-
+  singleClass.days.forEach((singleDay = element, dayIndex) => {
     response =
       "Setting subject for class " +
       singleClass.className +
@@ -61,6 +59,7 @@ classes.forEach((singleClass = element) => {
       singleDay.name;
     console.log(response);
 
+    // Making routin subjects (Step 1 started ) ############################
     let preiodNo = 0;
     singleClass.subs.forEach((singleSubject = element) => {
       if (preiodNo >= singleClass.classPerDay) {
@@ -76,11 +75,52 @@ classes.forEach((singleClass = element) => {
         }
       }
     });
+    // Making routin subjects ended (Step 1 done) ##########################
 
-    console.log(singleDay.sub);
+    // Now checking if overlapped day by day (Step 2 started ) #####################
+    if (classesIndex == 0) {
+      // console.log("No need to check overlapping !");
+    } else {
+      const loopLength = classesIndex;
+      for (let i = 0; i < loopLength; i++) {
+        // is overlapping
+        const checkingWith = classes[i].days[dayIndex].sub; // taking all classes one by one
+        checkingWith.forEach((checkingElement, checkingIndex) => {
+          if (checkingElement === singleDay.sub[checkingIndex]) {
+            //If matching then delete that index value and push it into last index of array
+            // console.log("Mached index no " + checkingIndex);
+            let temp = singleDay.sub.filter(
+              (item) => item !== singleDay.sub[checkingIndex]
+            );
+
+            temp.push(checkingElement);
+            singleDay.sub = temp;
+          }
+          checkingElement === singleDay.sub[checkingIndex]
+            ? console.log("Matched ! " + checkingElement)
+            : console.log("Not matched " + checkingElement);
+        });
+      }
+    }
+    // Now checking if overlapped day by day (Step 2 started ended ) #####################
+    // console.log(singleClass.days.sub);
   });
 
-  console.log("After remaining class");
-  console.log(singleClass.subs);
   //Check overlapping with everyclass
+});
+
+console.log("The Routine final result !");
+classes.forEach((element) => {
+  console.log("Class " + element.className + " Routin ");
+  element.days.forEach((dayElement = element) => {
+    console.log("Day :" + dayElement.name);
+    console.log("Subs :" + dayElement.sub);
+  });
+
+  console.log(
+    "Weekly remain class for class" +
+      element.className +
+      " ---------------------------------- "
+  );
+  console.log(element.subs);
 });
