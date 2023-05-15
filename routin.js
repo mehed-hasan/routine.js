f53f6a0698db75fbdde61a761a59bf935fec1904
mehed-hasan <mehedi.hasan@intelli.global>
2023-05-15 15:40:41 +0600

    Routin 1.3 solved version updated --!

--- routin.js
+++ routin.js
@@ -44,7 +44,6 @@ let classes = [
 
   //   ...
 ];
-
 let response = "";
 
 classes.forEach((singleClass = element, classesIndex) => {
@@ -52,13 +51,6 @@ classes.forEach((singleClass = element, classesIndex) => {
   //   -------------------------------------------------------
   // ----------------------------------------------------------
   singleClass.days.forEach((singleDay = element, dayIndex) => {
-    response =
-      "Setting subject for class " +
-      singleClass.className +
-      " for " +
-      singleDay.name;
-    console.log(response);
-
     // Making routin subjects (Step 1 started ) ############################
     let preiodNo = 0;
     singleClass.subs.forEach((singleSubject = element) => {
@@ -77,7 +69,7 @@ classes.forEach((singleClass = element, classesIndex) => {
     });
     // Making routin subjects ended (Step 1 done) ##########################
 
-    // Now checking if overlapped day by day (Step 2 started ) #####################
+    // Now checking if overlapped day by day (Step 2 started ) #####################----------------------------------------------------------------
     if (classesIndex == 0) {
       // console.log("No need to check overlapping !");
     } else {
@@ -86,41 +78,53 @@ classes.forEach((singleClass = element, classesIndex) => {
         // is overlapping
         const checkingWith = classes[i].days[dayIndex].sub; // taking all classes one by one
         checkingWith.forEach((checkingElement, checkingIndex) => {
+          console.log(
+            "Matching...... class " +
+              singleClass.className +
+              "  with class " +
+              classes[i].className
+          );
+          console.log(checkingElement + " VS " + singleDay.sub[checkingIndex]);
           if (checkingElement === singleDay.sub[checkingIndex]) {
             //If matching then delete that index value and push it into last index of array
             // console.log("Mached index no " + checkingIndex);
+            console.log("Matched ! " + checkingElement);
+
             let temp = singleDay.sub.filter(
               (item) => item !== singleDay.sub[checkingIndex]
             );
 
             temp.push(checkingElement);
             singleDay.sub = temp;
+          } else {
+            console.log(checkingElement + " Not matched ");
           }
-          checkingElement === singleDay.sub[checkingIndex]
-            ? console.log("Matched ! " + checkingElement)
-            : console.log("Not matched " + checkingElement);
         });
       }
+      console.log(
+        "================================\n================================\n================================\n\n\n"
+      );
     }
-    // Now checking if overlapped day by day (Step 2 started ended ) #####################
+    // Now checking if overlapped day by day (Step 2 started ended ) #####################---------------------------------------------------------
     // console.log(singleClass.days.sub);
   });
 
   //Check overlapping with everyclass
 });
 
-console.log("The Routine final result !");
+console.log("***********The Routine final result !**********************");
 classes.forEach((element) => {
-  console.log("Class " + element.className + " Routin ");
+  console.log("Class " + element.className + " routine ");
   element.days.forEach((dayElement = element) => {
-    console.log("Day :" + dayElement.name);
-    console.log("Subs :" + dayElement.sub);
+    console.log(dayElement.name + " ==> " + " " + dayElement.sub);
   });
 
-  console.log(
-    "Weekly remain class for class" +
-      element.className +
-      " ---------------------------------- "
-  );
-  console.log(element.subs);
+  //Weekly reaming started
+  // console.log(
+  //   "Weekly remain class for class" +
+  //     element.className +
+  //     " ---------------------------------- "
+  // );
+  // console.log(element.subs);
+  //Weekly reaming ended
 });
